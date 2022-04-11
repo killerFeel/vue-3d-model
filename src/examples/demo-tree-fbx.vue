2d9bdda768c0de048f7b74fae9035356de42bf1d
killerFeel <40201521@qq.com>
2022-04-11 12:26:10 +0800

    删除eslint代码检测工具1.对模型文件的坐标点统一化 （建议建模时对模型文件的初始大小设置稍大一点）2.优化vue-3d-model的使用方式更加方便3.对模型初始化展示y轴调整50°

---
+++ src/examples/demo-tree-fbx.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="exp">
+		<!-- 3D模型集成 -->
+		<model-fbx :backgroundColor="'#454e52'" :lights="lights" :backgroundAlpha="0.5" src="/models/tree/tree_01.fbx">
+		</model-fbx>
+	</div>
+</template>
+
+<script>
+	import {
+		ModelFbx
+	} from '@/vue-3d-model';
+
+	export default {
+		name: "exp",
+		components: {
+			ModelFbx
+		},
+		data() {
+			return {
+				lights: [{
+					type: 'HemisphereLight',
+					position: {
+						x: 0,
+						y: 1,
+						z: 0
+					},
+					skyColor: 0xffffff,
+					// groundColor: 0xFF0000, 此代码为灯光后颜色
+					intensity: 1,
+				}, {
+					type: 'DirectionalLight',
+					position: {
+						x: 1,
+						y: 1,
+						z: 1
+					},
+					color: 0xffffff,
+					intensity: .8,
+				}],
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
